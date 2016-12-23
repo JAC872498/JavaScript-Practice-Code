f0b5e067d9b56a6bbac5f3305aae9b9a691b63e0
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-22 18:30:16 -0600

    Update Finding Primes.js

--- Finding Primes.js
+++ Finding Primes.js
@@ -1,3 +1,19 @@
 /*This takes a number and finds all prime numbers up-to and including the number*/
 
-e
+function isPrime(num){
+  for(var x=2;x<num;x++){
+    if(num%x==0){
+      return(false);
+    }
+  }
+  return(true);
+}
+
+function findingPrime(num){
+   for(var x=2;x<=num;x++){
+     if(isPrime(x)){
+       console.log(x);
+     }
+   }
+ }
+findingPrime(23);
