c408abd36e23d4811559ed2a45a695444daec11f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-01 14:41:15 -0600

    Update Coin Counter.js

--- Coin Counter.js
+++ Coin Counter.js
@@ -1,13 +1,18 @@
- /*This takes a price and gives the greates number of coins that can be used to make the price
- 
- Sample function : amountTocoins(46, [25, 10, 5, 2, 1])
+ /*Sample function : amountTocoins(46, [25, 10, 5, 2, 1])
 Here 46 is the amount. and 25, 10, 5, 2, 1 are coins. 
 Output : 25, 10, 10, 1*/
 
 function coin(total, coins){
+  var t;
   while(total>0){
-    console.log(total);
-    total-=coins[4];
+    top=0;
+    for(var x=0;x<=coins.length;x++){
+      if(coins[x]<=total){
+        t=coins[x];
+      }
+    }
+    total-=t;
+    console.log("top:"+t+" total:"+total);
   }
 }
-coin(46,[25, 10, 5, 2, 1]);
+coin(46,[1,5,10,25])
