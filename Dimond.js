a8abcca5087615846fd6365a3db676c5df65f1e8
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-21 21:32:05 -0600

    Update Dimond.js

--- Dimond.js
+++ Dimond.js
@@ -1,8 +1,14 @@
 var wrd="";
 for(var x=0;x<5;x++){
   for(var y=0;y<=x;y++){
-    wrd=wrd+"*";
+    wrd+="*";
   }
-  wrd=wrd+"\n";
+  wrd+="\n";
+}
+for(var x=5;x>0;x--){
+  for(var y=0;y<=x;y++){
+    wrd+="*";
+  }
+  wrd+="\n";
 }
 console.log(wrd);
