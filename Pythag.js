e526c633e650c21aa0108be22cc15bb318d9e54a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-30 11:17:30 -0600

    Update Pythag.js

--- Pythag.js
+++ Pythag.js
@@ -2,13 +2,13 @@ var choice=prompt("Enter 1 if you don't have side A, 2 if you don't have side B,
 if(choice==1){
 	var C=prompt("Enter side C.");
 	var B=prompt("Enter side B.");
-	console.log("Side A is "+(Math.sqrt(C*2-B*2))+".");
+	console.log("Side A is "+(Math.sqrt(C*C-B*B))+".");
 }else if(choice==2){
 	var A=prompt("Enter side A.");
 	var C=prompt("Enter side C.");
-	console.log("Side B is "+(Math.sqrt(C*2-A*2))+".");
+	console.log("Side B is "+(Math.sqrt(C*C-A*A))+".");
 }else if(choice==3){
 	var A=prompt("Enter side A.");
 	var B=prompt("Enter side B.");
-	console.log("Side C is "+(Math.sqrt(B*2+A*2))+".");
+	console.log("Side C is "+(Math.sqrt(B*B+A*A))+".");
 }
