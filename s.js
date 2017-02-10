4da2aa8c720460c0c184bddf08b9ddc2a6c897f9
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-10 08:20:41 -0600

    Update s.js

--- s.js
+++ s.js
@@ -1,4 +1,8 @@
 var num=120;
 var zeros=5;
-var newNum=0;
-console.log(zeros-(num.toString()).length);
+var newNum=120;
+var s=zeros-(num.toString()).length;
+for(var x=0;x<s;x++){
+  s/=10;
+}
+console.log(s);
