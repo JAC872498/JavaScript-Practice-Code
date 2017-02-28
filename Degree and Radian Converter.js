3c62514177ad053be23d23c24ab8df6c2f714c73
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-28 12:37:23 -0600

    Update Degree and Radian Converter.js

--- Degree and Radian Converter.js
+++ Degree and Radian Converter.js
@@ -1,3 +1,12 @@
 /*This either converts a radian number to a degree or vice versa*/
+function simp(){
+  return [0,1];
+}
 
-var num=prompt("Enter 1 for degree->radian and 2 for radian->degree");
+var choice=prompt("Enter 1 for degree->radian and 2 for radian->degree.");
+var num=prompt("Enter the starting number.");
+if(choice==1){//degree->radian
+  return()
+}else{//radian->degree
+  
+}
