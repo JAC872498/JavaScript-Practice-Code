f6a0b94da18dd9ac8b45c45a41a698def417652f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-01 10:02:49 -0600

    Update Radical Simplifier.js

--- Radical Simplifier.js
+++ Radical Simplifier.js
@@ -1,8 +1,10 @@
-/**/
+ /**/
 var preN=prompt("");
 var mainN=prompt("");
-for(var x=mainNN;x>0;x++){
-  if(Math.sqrt(x)%1==0){
-    console.log(x);
+for(var x=mainN-1;x>=0;x--){
+  if(Math.sqrt(x)%1==0 && (mainN/(x))%1==0){
+    console.log(x+0);
+    console.log(mainN/(x));
+    break;
   }
 }
