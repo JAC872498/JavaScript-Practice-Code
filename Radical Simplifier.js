c13b906be611f3cc514c93a6251f03ec3b4586c3
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-01 11:00:14 -0600

    Update Radical Simplifier.js

--- Radical Simplifier.js
+++ Radical Simplifier.js
@@ -1,12 +1,12 @@
-/**/
-var preN=prompt("");
-var mainN=prompt("");
+/*This takes an inputed radical and returns it's simplest form*/
+var preN=prompt("Enter what the radical's being multiplied by.");
+var mainN=prompt("Enter the radicand.");
 for(var x=mainN-1;x>=0;x--){
   if(Math.sqrt(x)%1==0 && (mainN/(x))%1==0){
     if(Math.sqrt(mainN/x)%1==0){
-      console.log(preN+"root"+mainN+" can be simplified to "+(Math.sqrt(x)*preN*Math.sqrt(mainN/x)));
+      console.log(preN+"√"+mainN+" can be simplified to "+(Math.sqrt(x)*preN*Math.sqrt(mainN/x)));
     }else{
-      console.log(preN+"root"+mainN+" can be simplified to "+(Math.sqrt(x)*preN)+" root"+mainN/x);
+      console.log(preN+"√"+mainN+" can be simplified to "+(Math.sqrt(x)*preN)+"√"+mainN/x);
     }
     break;
   }
