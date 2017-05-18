ccea92e0cfa74099933479a9c542ed3c9fea4e59
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-18 15:40:32 -0500

    Update Robot Simulator.js

--- Robot Simulator.js
+++ Robot Simulator.js
@@ -1,6 +1,6 @@
 /**/
 
-var directions["west","north","east","south"];
-var coords={0,0};
+var directions=["west","north","east","south"];
+var coords=[0,0];
 var direction=directions[1];
 var instructions=prompt("Enter the instructions.");
