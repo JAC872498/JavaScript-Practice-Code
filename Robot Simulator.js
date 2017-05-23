adadb4ee7c3dcad047d7f053919417f3f664e061
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-23 15:28:10 -0500

    Update Robot Simulator.js

--- Robot Simulator.js
+++ Robot Simulator.js
@@ -5,8 +5,7 @@ var coords=[0,0];
 var direction=directions[1];
 var instructions=prompt("Enter the instructions.");
 for(int x=0;x<;x++){
-  console.log("The robot is at",coors[0]
-              
+  console.log("The robot is at",coors[0],", ",coors[1];              
   if(R){
     
   }else if(L){
