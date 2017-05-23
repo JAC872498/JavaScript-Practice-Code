dd7c42e1c3d9a919281eb1389e9cc8ed1d3c946a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-23 15:39:23 -0500

    Update Robot Simulator.js

--- Robot Simulator.js
+++ Robot Simulator.js
@@ -2,15 +2,32 @@
 
 var directions=["west","north","east","south"];
 var coords=[0,0];
-var direction=directions[1];
+var currDir=1;
+var direction=directions[currDir];
 var instructions=prompt("Enter the instructions.").split("");
 for(int x=0;x<instructions.length;x++){
-  console.log("The robot is at",coors[0],", ",coors[1];              
+  console.log("The robot is at",coors[0],", ",coors[1]," and is facing ",direction;              
   if(R){
-    
+    currDir+=1;
+    if(currDir==4){
+      currDir=0;
+    }
+    direction=directions[currDur];
   }else if(L){
-    
+    currDir-=1;
+    if(currDir==-1){
+      currDir=3;
+    }
+    direction=directions[currDur];
   }else if(A){
-    
+    if(currDur==0){
+      
+    }else if(currDur==1){
+      
+    }else if(currDur==2){
+      
+    }else if(currDur==3){
+      
+    }
   }
 }
