4d99392ae9a5c7b67b9e679ea356363e6865312b
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-05 19:42:49 -0500

    Update Series.js

--- Series.js
+++ Series.js
@@ -1,7 +1,15 @@
-/**/
+/*Given a string of digits, output all the contiguous substrings of length n in that string.
 
-wrd=""
+For example, the string "49142" has the following 3-digit series:
+491
+914
+142
+And the following 4-digit series:
+4914
+9142*/
+
+wrd="abc"
 n=1;
-for(var x=0;x<wrd.length()-n;x++){
+for(var x=0;x<=wrd.length-n;x++){
   console.log(wrd.substring(x,x+n));
 }
