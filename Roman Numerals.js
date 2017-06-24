b22086a0849c1a2c4da26c69bf01003ac949258e
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-23 21:05:55 -0500

    Update Roman Numerals.js

--- Roman Numerals.js
+++ Roman Numerals.js
@@ -8,17 +8,19 @@ var used=0;
 while(num>0){
   for(var x=6;x>=0;x--){
     //console.log(signs[x],vals[x]);
-    if(vals[x]<=num&&used<3){
+    if(vals[x]<=num&&used<2){
       newNum+=signs[x];
       num-=vals[x];
       if(signs[x]===newNum.split("")[newNum.length-2]){
         used++;
-        console.log(used);
+        //console.log(used);
       }else{
         used=0;
-      }console.log(newNum);
+      }//console.log(newNum);
     }else if(vals[x]<=num){
       used=0;
+      num+=(vals[x]*3);
+      newNum=newNum.substring(0,newNum.lenth-3);
       newNum+=signs[x+1]+signs[x];
       num-=(vals[x+1]-vals[x]);
     }
