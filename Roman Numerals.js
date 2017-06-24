5acad0220eee126961765ea1e8acd866da33c955
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-23 20:13:01 -0500

    Update Roman Numerals.js

--- Roman Numerals.js
+++ Roman Numerals.js
@@ -4,12 +4,15 @@ var num=parseInt(prompt("Enter number."));
 var newNum="";
 var vals=[1,5,10,50,100,500,1000];
 var signs=["I","V","X","L","C","D","M"];
+var used=0;
 while(num>0){
   for(var x=6;x>=0;x--){
     //console.log(signs[x],vals[x]);
-    if(vals[x]<=num){
+    if(vals[x]<=num&&used<3){
       newNum+=signs[x];
       num-=vals[x];
+    }else if(vals[x]<=num){
+      
     }
   }
 }
