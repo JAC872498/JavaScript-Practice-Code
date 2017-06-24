e5a20614e7bbf3433b410da6ece351c4fc11d20d
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-24 00:05:27 -0500

    Update Roman Numerals.js

--- Roman Numerals.js
+++ Roman Numerals.js
@@ -1,4 +1,7 @@
-/**/
+/*This takes a number and returns it in roman numerals
+does it work 100%? probably not
+could it be optimized and cleaned up? definitely
+are there better places to do this? lots of them*/
 
 function getVal(a){
   if(a==1||a==="I"){
@@ -23,6 +26,22 @@ var newNum="";
 var vals=[1,5,10,50,100,500,1000];
 var signs=["I","V","X","L","C","D","M"];
 while(num>0){
+  if(num==9){
+    num-=9;
+    newNum+="IX";
+  }else if(num==49){
+    num-=49;
+    newNum+="XLIX";
+  }else if(num==99){
+    num-=99;
+    newNum+="XCIX";
+  }else if(num==499){
+    num-=499;
+    newNum+="CDXCIX";
+  }else if(num==999){
+    num-=999;
+    newNum+="CMXCIX";
+  }
   for(var x=6;x>=0;x--){
     //console.log(signs[x],vals[x]);
     if(vals[x]<=num){
