ea051b12182aaf580d77cacf73e68a4c09e12d63
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-23 21:28:23 -0500

    Update Roman Numerals.js

--- Roman Numerals.js
+++ Roman Numerals.js
@@ -1,29 +1,37 @@
 /**/
 
+function getVal(a){
+  if(a==1){
+    return 1;
+  }else if(a==5){
+    
+  }else if(a==10){
+    
+  }else if(a==50){
+    
+  }else if(a==100){
+    
+  }else if(a==500){
+    
+  }else if(a==1000){
+    
+  }
+}
+
 var num=parseInt(prompt("Enter number."));
 var newNum="";
 var vals=[1,5,10,50,100,500,1000];
 var signs=["I","V","X","L","C","D","M"];
-var used=0;
 while(num>0){
   for(var x=6;x>=0;x--){
     //console.log(signs[x],vals[x]);
-    if(vals[x]<=num&&used<2){
+    if(vals[x]<=num){
       newNum+=signs[x];
       num-=vals[x];
-      if(signs[x]===newNum.split("")[newNum.length-2]){
-        used++;
-        //console.log(used);
-      }else{
-        used=0;
-      }//console.log(newNum);
-    }else if(vals[x]<=num){
-      used=0;
-      num+=(vals[x]*3);
-      newNum=newNum.substring(0,newNum.lenth-3);
-      newNum+=signs[x+1]+signs[x];
-      num-=(vals[x+1]-vals[x]);
     }
+  }
+  if(newNum.length>=4&&newNum.substring(newNum.length-1,newNum.length)===newNum.substring(newNum.length-2,newNum.length-1)&&newNum.substring(newNum.length-2,newNum.length)===newNum.substring(newNum.length-4,newNum.length-2)){
+    console.log("Too long");
   }
 }
 console.log(newNum);
