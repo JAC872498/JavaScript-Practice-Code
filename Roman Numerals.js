f6b8878224e419ce311e45eaff7f8183833b7b25
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-23 23:58:33 -0500

    Update Roman Numerals.js

--- Roman Numerals.js
+++ Roman Numerals.js
@@ -1,20 +1,20 @@
 /**/
 
 function getVal(a){
-  if(a==1){
+  if(a==1||a==="I"){
+    return 0;
+  }else if(a==5||a==="V"){
     return 1;
-  }else if(a==5){
-    
-  }else if(a==10){
-    
-  }else if(a==50){
-    
-  }else if(a==100){
-    
-  }else if(a==500){
-    
-  }else if(a==1000){
-    
+  }else if(a==10||a==="X"){
+    return 2;
+  }else if(a==50||a==="L"){
+    return 3;
+  }else if(a==100||a=="C"){
+    return 4;
+  }else if(a==500||a==="D"){
+    return 5;
+  }else if(a==1000||a==="M"){
+    return 6;
   }
 }
 
@@ -31,7 +31,10 @@ while(num>0){
     }
   }
   if(newNum.length>=4&&newNum.substring(newNum.length-1,newNum.length)===newNum.substring(newNum.length-2,newNum.length-1)&&newNum.substring(newNum.length-2,newNum.length)===newNum.substring(newNum.length-4,newNum.length-2)){
-    console.log("Too long");
+    //console.log("Too long");
+    n=getVal(newNum.substring(newNum.length-1,newNum.length));
+    newNum=newNum.substring(0,newNum.length-4);
+    newNum+=signs[n]+signs[n+1];
   }
 }
 console.log(newNum);
