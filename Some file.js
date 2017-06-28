f1de6fb5714dd8738bf522132fff87d3ddd0d59a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-28 17:35:58 -0500

    Update Some file.js

--- Some file.js
+++ Some file.js
@@ -1,1 +1,23 @@
 /**/
+
+function change(wrd,n){
+  var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  for(var i=0;i<alph.length;i++){
+    if(wrd===alph[i]){
+      var num=i;
+      break;
+    }
+  }
+  num+=n;
+  while(num>26){
+    num-=26;
+  }
+  return(num);
+}
+
+function cipher1(wrd,n){
+  var newWrd="";
+  for(var x=0;x<wrd.split('').length;x++){
+    newWrd=newWrd+change(wrd.split('')[x],n)
+  }
+}
