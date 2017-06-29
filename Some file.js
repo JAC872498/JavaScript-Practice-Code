243f968262ed38747e3c61aac872cb8cf5dfd5b5
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-28 19:40:08 -0500

    Update Some file.js

--- Some file.js
+++ Some file.js
@@ -8,23 +8,19 @@ function getVal(letter,alph){
   }
 }
 
-function cipher1(wrd,n){
+function cipher(wrd,n,key){
   var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var newWrd="";
   var num=0;
   for(var x=0;x<wrd.split('').length;x++){
     num=getVal(wrd.split('')[x],alph);
-    num+=n;
+    num+=n+getVal(key.split('')[x],alph);
     while(num>=26){
       num-=26;
     }
     newWrd=newWrd+alph[num];
-    
   }
   return(newWrd);
 }
-function cipher2(wrd,key){
-  
-}
 
-console.log(cipher1("abcdefghijklmnopqrstuvwxyz",1))
+console.log(cipher("abc",0,"abc"));
