bb9b96258af50c9512f82d0aee96da96953be4bd
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-28 19:46:36 -0500

    Update Some file.js

--- Some file.js
+++ Some file.js
@@ -6,9 +6,10 @@ function getVal(letter,alph){
       return i;
     }
   }
+  return(69);
 }
 
-function cipher(wrd,n,key){
+function encrypt(wrd,n,key){
   var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var newWrd="";
   var num=0;
@@ -22,5 +23,20 @@ function cipher(wrd,n,key){
   }
   return(newWrd);
 }
+function decrypt(wrd,n,key){
+  var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var newWrd="";
+  var num=0;
+  for(var x=0;x<wrd.split('').length;x++){
+    num=getVal(wrd.split('')[x],alph);
+    num-=n+getVal(key.split('')[x],alph);
+    while(num<0){
+      num+=26;
+    }
+    newWrd=newWrd+alph[num];
+  }
+  return(newWrd);
+}
 
-console.log(cipher("abc",0,"abc"));
+console.log(encrypt("abc",12345,"nab"));
+console.log(decrypt("iwy",12345,"nab"));
