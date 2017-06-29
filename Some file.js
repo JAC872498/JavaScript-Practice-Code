e7be9e1a6df09e9452313a4dc116e98f3cf8a8b6
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-28 19:32:27 -0500

    Update Some file.js

--- Some file.js
+++ Some file.js
@@ -1,26 +1,30 @@
 /**/
 
-function change(wrd,n){
-  var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+function getVal(letter,alph){
   for(var i=0;i<alph.length;i++){
-    if(wrd===alph[i]){
-      var num=i;
-      break;
+    if(letter.toLowerCase()===alph[i]){
+      return i;
     }
   }
-  num+=n;
-  while(num>=26){
-    num-=26;
-  }
-  return(alph[num]);
 }
 
 function cipher1(wrd,n){
+  var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var newWrd="";
+  var num=0;
   for(var x=0;x<wrd.split('').length;x++){
-    newWrd=newWrd+change(wrd.split('')[x],n);
+    num=getVal(wrd.split('')[x],alph);
+    num+=n;
+    while(num>=26){
+      num-=26;
+    }
+    newWrd=newWrd+alph[num];
+    
   }
   return(newWrd);
 }
+function cipher2(wrd,key){
+  
+}
 
-console.log(cipher1("abc",26))
+console.log(cipher1("abcdefghijklmnopqrstuvwxyz",1))
