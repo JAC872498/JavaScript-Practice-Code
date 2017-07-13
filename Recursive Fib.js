97cfbf5d48fc05a59f0de210b06c0e22781a0824
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-12 21:11:27 -0500

    Update Recursive Fib.js

--- Recursive Fib.js
+++ Recursive Fib.js
@@ -1,8 +1,8 @@
-/**/
+/*This dispalays a requested number of digits in the Fibonacci sequence recursivley*/
 
 function fib(n,nums=[0,1]){
   if(n<=2) return(nums);
   nums.push(nums[nums.length-1]+nums[nums.length-2]);
   return(fib(n-1,nums));
 }
-console.log(fib(1));
+console.log(fib(10));
