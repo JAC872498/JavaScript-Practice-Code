48d50d5fcc137a5618a8386a24561005ba4fa3db
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-02 15:15:10 -0500

    Update asdfasdf.js

--- asdfasdf.js
+++ asdfasdf.js
@@ -5,8 +5,8 @@ var newNum=0;
 var base=2;
 for(var n=0;n<number;n++){
   newNum+=1;
-  for(var x=1;x<=(newNumber.toString()).length();x++){
-    if(newNumber%x==base){
+  for(var x=1;x<=(newNum.toString()).length();x++){
+    if(newNum%x==base){
       newNum-=(base*(10**x));
       newNum+=(10**(x+1));
     }
