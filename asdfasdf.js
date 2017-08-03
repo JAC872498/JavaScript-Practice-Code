322a98c6d704763714a968eb83741cbf17504363
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-02 21:47:16 -0500

    Update asdfasdf.js

--- asdfasdf.js
+++ asdfasdf.js
@@ -1,16 +1,16 @@
 /**/
 
-var number=123;
+var number=20;
 var newNum=0;
 var base=2;
 for(var n=0;n<number;n++){
   newNum+=1;
-  for(var x=1;x<=(test1.toString()).length;x++){
+  for(var x=1;x<=(number.toString()).length;x++){
   //console.log(Math.floor((test1%(10**x)/(10**(x-1)))));
-    if(Math.floor((test1%(10**x)/(10**(x-1))))>=base){
+    if(Math.floor((newNum%(10**x)/(10**(x-1))))>=base){
     //console.log(Math.floor((test1%(10**x)/(10**(x-1)))));
-      test1-=(10**(x-1)*base);
-      test1+=(10**x);
+      newNum-=(10**(x-1)*base);
+      newNum+=(10**x);
     }
   }
 }
