82dedf045124f486abe933f05c9ab7791ff58dbe
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-11 12:02:01 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -1,2 +1,13 @@
-/*This is a mega-collection of word ciphers, ranging from weak ones you use with friends to extreme ones used by militaries*/
-
+/*This is a mega-collection of word ciphers, ranging from weak ones you use with friends to extreme ones used by militaries
+below is a list of the ciphers that numbers corespond to, along with an explination of the cipher
+0-
+1-
+2-
+3-
+4-
+5-
+6-
+7-
+8-
+9-
+10-*/
