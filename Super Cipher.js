5c866080df397a48fc526b07fba37191d9539e2e
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-11 12:14:31 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -1,8 +1,8 @@
 /*This is a mega-collection of word ciphers, ranging from weak ones you use with friends to extreme ones used by militaries
 below is a list of the ciphers that numbers corespond to, along with an explination of the cipher
-0-
-1-
-2-
+0- four square cipher http://practicalcryptography.com/ciphers/classical-era/four-square/
+1- polybius square http://practicalcryptography.com/ciphers/classical-era/polybius-square/
+2- ROT13 http://practicalcryptography.com/ciphers/classical-era/rot13/
 3-
 4-
 5-
