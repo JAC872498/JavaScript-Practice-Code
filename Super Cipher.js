997cc81965bac108bc159143eff8df28d56c2395
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-12 11:54:55 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -12,8 +12,10 @@ below is a list of the ciphers that numbers corespond to, along with an explinat
 9-
 10-*/
 
-function enigma(){
+function enigma(wrd,pos){
+  var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var rotor1=["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"];
-  var rotor2=[AJDKSIRUXBLHWTMCQGZNPYFVOE];
-  var rotor3=[BDFHJLCPRTXVZNYEIWGAKMUSQO];
+  var rotor2=["A","J","D","K","S","I","R","U","X","B","L","H","W","T","M","C","Q","G","Z","N","P","Y","F","V","O","E"];
+  var rotor3=["B","D","F","H","J","L","C","P","R","T","X","V","Z","N","Y","E","I","W","G","A","K","M","U","S","Q","O"];
 }
+
