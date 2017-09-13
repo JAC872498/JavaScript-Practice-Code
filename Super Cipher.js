e73cb6a3d3c6801393c112bcc6201bdfc19b6a56
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-13 11:50:39 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -13,7 +13,8 @@ below is a list of the ciphers that numbers corespond to, along with an explinat
 10-*/
 
 function fourSquare(wrd){
-  
+	var alph=[["A","B","C","D","E"],["F","G","H","I","J"],["K","L","M","N","O"],["P","R","S","T","U"],["V","W","X","Y","Z"]];
+
 }
 
 function polybius(wrd){
@@ -21,14 +22,14 @@ function polybius(wrd){
 }
 
 function ROT13(wrd){
-  
+	
 }
 
 function enigma(wrd,pos){
-  var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var rotor1=["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"];
-  var rotor2=["A","J","D","K","S","I","R","U","X","B","L","H","W","T","M","C","Q","G","Z","N","P","Y","F","V","O","E"];
-  var rotor3=["B","D","F","H","J","L","C","P","R","T","X","V","Z","N","Y","E","I","W","G","A","K","M","U","S","Q","O"];
+	var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+	var rotor1=["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"];
+	var rotor2=["A","J","D","K","S","I","R","U","X","B","L","H","W","T","M","C","Q","G","Z","N","P","Y","F","V","O","E"];
+	var rotor3=["B","D","F","H","J","L","C","P","R","T","X","V","Z","N","Y","E","I","W","G","A","K","M","U","S","Q","O"];
 }
 
 function lorenz(wrd){
