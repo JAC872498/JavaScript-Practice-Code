c6be1daedbf79d5148ddcaad6fc37fdb0255438e
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-12 23:26:29 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -20,6 +20,10 @@ function polybius(wrd){
   
 }
 
+function ROT13(wrd){
+  
+}
+
 function enigma(wrd,pos){
   var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var rotor1=["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"];
