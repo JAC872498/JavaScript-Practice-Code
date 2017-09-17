404bb57cb15d7b24812ede567d52ab36287bc58d
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-17 18:35:07 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -114,12 +114,15 @@ function ROT13(wrd,rot){//DONE
 	return("Encoded:"+newWrdE+"\nDecoded:"+newWrdD);
 }
 
-function enigma(wrd,pos){//NOT DONE
+function enigma(wrd){//NOT DONE
+    wrd=wrd.toUpperCase();
+    wrd=wrd.split("");
+    var newWrd="";
 	var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 	var rotor1=["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"];
 	var rotor2=["A","J","D","K","S","I","R","U","X","B","L","H","W","T","M","C","Q","G","Z","N","P","Y","F","V","O","E"];
 	var rotor3=["B","D","F","H","J","L","C","P","R","T","X","V","Z","N","Y","E","I","W","G","A","K","M","U","S","Q","O"];
-	var reflectorA=["E","J","M","Z","A","L","Y","X","V","B","W","F","C","R","Q","U","O","N","T","S","P","I","K","H","G","D"];
+    var reflectorA=["E","J","M","Z","A","L","Y","X","V","B","W","F","C","R","Q","U","O","N","T","S","P","I","K","H","G","D"];
 }
 
 function lorenz(wrd){//NOT DONE
