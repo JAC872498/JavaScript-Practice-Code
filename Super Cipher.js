ee495583b09776203337a05ecd01a3e7b3e346f4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-17 18:10:47 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -119,7 +119,7 @@ function enigma(wrd,pos){//NOT DONE
 	var rotor1=["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"];
 	var rotor2=["A","J","D","K","S","I","R","U","X","B","L","H","W","T","M","C","Q","G","Z","N","P","Y","F","V","O","E"];
 	var rotor3=["B","D","F","H","J","L","C","P","R","T","X","V","Z","N","Y","E","I","W","G","A","K","M","U","S","Q","O"];
-	var reflectorA=["EJMZALYXVBWFCRQUONTSPIKHGD"];
+	var reflectorA=["E","J","M","Z","A","L","Y","X","V","B","W","F","C","R","Q","U","O","N","T","S","P","I","K","H","G","D"];
 }
 
 function lorenz(wrd){//NOT DONE
