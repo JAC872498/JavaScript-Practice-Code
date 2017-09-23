2be924b074d26f801f1b2c52f37aaf49496b42cd
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-22 21:54:01 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -3,9 +3,10 @@ below is a list of the ciphers that numbers corespond to, along with an explinat
 0- four square cipher http://practicalcryptography.com/ciphers/classical-era/four-square/
 1- polybius square http://practicalcryptography.com/ciphers/classical-era/polybius-square/
 2- ROT13 http://practicalcryptography.com/ciphers/classical-era/rot13/
+3- keyword cipher https://en.wikipedia.org/wiki/Keyword_cipher
 3- enigma cipher http://practicalcryptography.com/ciphers/mechanical-era/enigma/
 4- lorenz cipher http://practicalcryptography.com/ciphers/mechanical-era/lorenz/
-5-
+5- keyword cipher
 6-
 7-
 8-
