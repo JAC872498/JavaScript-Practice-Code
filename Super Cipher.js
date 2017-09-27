f252e9e4d3518be5f8a0eef2f7397b3447997e91
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-26 20:58:52 -0500

    Update Super Cipher.js

--- Super Cipher.js
+++ Super Cipher.js
@@ -3,12 +3,11 @@ below is a list of the ciphers that numbers corespond to, along with an explinat
 0- 'leet speek', not really a cipher as much as it is a joke translator https://en.wikipedia.org/wiki/Leet
 1- morse code, also not much of a cipher but still one I wanted to include
 2- four square cipher http://practicalcryptography.com/ciphers/classical-era/four-square/
-2- polybius square http://practicalcryptography.com/ciphers/classical-era/polybius-square/
-3- ROT13 http://practicalcryptography.com/ciphers/classical-era/rot13/
-4- keyword cipher https://en.wikipedia.org/wiki/Keyword_cipher
-5- enigma cipher http://practicalcryptography.com/ciphers/mechanical-era/enigma/
-6- lorenz cipher http://practicalcryptography.com/ciphers/mechanical-era/lorenz/
-7-
+3- polybius square http://practicalcryptography.com/ciphers/classical-era/polybius-square/
+4- ROT13 http://practicalcryptography.com/ciphers/classical-era/rot13/
+5- keyword cipher https://en.wikipedia.org/wiki/Keyword_cipher
+6- enigma cipher http://practicalcryptography.com/ciphers/mechanical-era/enigma/
+7- lorenz cipher http://practicalcryptography.com/ciphers/mechanical-era/lorenz/
 8-
 9-
 10-
