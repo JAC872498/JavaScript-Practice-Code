a7ff2ecf8ad40a5c04311c1b7d0fb62d1ed87bb6
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-29 14:13:13 -0500

    Update Super Cipher Decoding.js

--- Super Cipher Decoding.js
+++ Super Cipher Decoding.js
@@ -13,3 +13,23 @@ below is a list of the ciphers that numbers corespond to, along with an explinat
 9-
 10-
 Created by Jordan Camilletti*/
+
+function findAlph(letter,alph,dimensions){//this is used to find the number location of letters(A=0,B=1,C=2,G=7) or (A=[0,0],B=[0,1],C=[0,2],G=[1,0])
+	letter=letter.toUpperCase();
+	var location=[]
+	if(dimensions==1){
+		for(var x=0;x<alph.length;x++){
+			if(alph[x]==letter) location.push(x);
+		}
+	}else if(dimensions==2){
+		for(var y=0;y<alph.length;y++){
+			for(var x=0;x<alph[y].length;x++){
+				if(alph[y][x]==letter){
+					location.push(y);
+					location.push(x);
+				}
+			}
+		}
+	}
+	return location;
+}
