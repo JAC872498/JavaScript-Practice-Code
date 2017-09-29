31bb2d78c08a6067b4a3317a79968cdd3e3a5274
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-28 22:40:03 -0500

    Update Super Cipher Encoding.js

--- Super Cipher Encoding.js
+++ Super Cipher Encoding.js
@@ -117,24 +117,19 @@ function polybius(wrd){//DONE
 
 
 function ROT13(wrd,rot){//DONE
-	var newWrdE="",newWrdD="";
+	var newWrd="";
 	wrd=wrd.toUpperCase();
 	wrd=wrd.split("");
 	var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]; 
 	for(var char=0;char<wrd.length;char++){
 		var x=findAlph(wrd[char],alph,1)[0];
 		if(x+rot>35){
-			newWrdE=newWrdE+alph[x-rot];
+			newWrd=newWrd+alph[x-rot];
 		}else{
-			newWrdE=newWrdE+alph[x+rot];
-		}
-		if(x-rot<0){
-			newWrdD=newWrdD+alph[x+rot];
-		}else{
-			newWrdD=newWrdD+alph[x-rot];
+			newWrd=newWrd+alph[x+rot];
 		}
 	}
-	return("Encoded:"+newWrdE+"\nDecoded:"+newWrdD);
+	return(newWrd);
 } 
 
 function keyWord(wrd,keyWrd){//DONE
