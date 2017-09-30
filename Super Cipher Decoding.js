0ff06a53a17697d0e16fd602d5616fb21a82fa1f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-30 17:26:22 -0500

    Update Super Cipher Decoding.js

--- Super Cipher Decoding.js
+++ Super Cipher Decoding.js
@@ -3,13 +3,13 @@ This file is used to decode words
 below is a list of the ciphers that numbers corespond to, along with an explination of the cipher
 0- 'leet speek', not really a cipher as much as it is a joke translator https://en.wikipedia.org/wiki/Leet
 1- morse code, also not much of a cipher but still one I wanted to include https://morsecode.scphillips.com/morse2.html
-2- four square cipher http://practicalcryptography.com/ciphers/classical-era/four-square/
-3- polybius square http://practicalcryptography.com/ciphers/classical-era/polybius-square/
-4- ROT13 http://practicalcryptography.com/ciphers/classical-era/rot13/
-5- keyword cipher https://en.wikipedia.org/wiki/Keyword_cipher
-6- enigma cipher http://practicalcryptography.com/ciphers/mechanical-era/enigma/
-7- lorenz cipher http://practicalcryptography.com/ciphers/mechanical-era/lorenz/
-8-
+2- pig latin
+3- four square cipher http://practicalcryptography.com/ciphers/classical-era/four-square/
+4- polybius square http://practicalcryptography.com/ciphers/classical-era/polybius-square/
+5- ROT13 http://practicalcryptography.com/ciphers/classical-era/rot13/
+6- keyword cipher https://en.wikipedia.org/wiki/Keyword_cipher
+7- enigma cipher http://practicalcryptography.com/ciphers/mechanical-era/enigma/
+8- lorenz cipher http://practicalcryptography.com/ciphers/mechanical-era/lorenz/
 9-
 Created by Jordan Camilletti*/
 
