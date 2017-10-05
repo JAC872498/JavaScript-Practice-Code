2fea04ad6a5c23d505ea1c707a8f8a68b6d63000
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-05 12:04:43 -0500

    Update Super Cipher Decoding.js

--- Super Cipher Decoding.js
+++ Super Cipher Decoding.js
@@ -103,6 +103,17 @@ function ROT13(wrd,rot){//DONE
 function keyWord(wrd,keyWrd){//NOT DONE
 	wrd=wrd.toUpperCase();
 	wrd=wrd.split("");
+	keyWrd=keyWrd.toUpperCase();
+	keyWrd=keyWrd.split("");
+	var newWrd="";
+	var alph=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"];
+	if(keyWrd.length<36){
+		for(var letter in alph){
+			if(!keyWrd.includes(alph[letter])){
+				keyWrd.push(alph[letter]);
+			}
+		}
+	}
 }
 
 function ADFGVX(wrd){//NOT DONE
