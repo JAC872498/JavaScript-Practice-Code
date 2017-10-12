c36b084b76acee3e4cb7d86e65cf1a0561179efe
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-12 08:58:17 -0500

    Update Super Cipher Decoding.js

--- Super Cipher Decoding.js
+++ Super Cipher Decoding.js
@@ -169,7 +169,7 @@ function ADFGVX(wrd,keySquare,keyWrd,keyWrd2=keyWrd){//NOT DONE
 		}
 	}
 	for(char in sortedWrd[5]){
-		newWrd=newWrd+sortedWrd[0][char]+sortedWrd[1][char]+sortedWrd[2][char]+sortedWrd[3][char]+sortedWrd[4][char]+sortedWrd[5][char]
+		newWrd=newWrd+sortedWrd[0][char]+sortedWrd[1][char]+sortedWrd[2][char]+sortedWrd[3][char]+sortedWrd[4][char]+sortedWrd[5][char];
 	}
 	for(char=0;char<=longest;char++){//Both of these loops take the letters out of column form and put them in a single string
 		newWrd=newWrd+sortedWrd[char][[sortedWrd[char].length]-1];
