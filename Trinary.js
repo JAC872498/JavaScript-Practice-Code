93589e29401be2abef2acead22511878230c9724
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-15 09:08:10 -0600

    Update Trinary.js

--- Trinary.js
+++ Trinary.js
@@ -1,1 +1,7 @@
 /*http://exercism.io/exercises/javascript/trinary/readme*/
+
+var num=12345;
+var power=1;
+for(var n=2;n<=6;n++){
+  console.log(Math.ceil((num%Math.pow(10,n))/Math.pow(10,n-1)));
+}
