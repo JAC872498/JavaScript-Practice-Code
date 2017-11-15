2817450190644694fb124f650194b296e702edf3
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-15 11:23:35 -0600

    Update Trinary.js

--- Trinary.js
+++ Trinary.js
@@ -2,6 +2,6 @@
 
 var num=12345;
 var power=1;
-for(var n=2;n<=6;n++){
-  console.log(Math.ceil((num%Math.pow(10,n))/Math.pow(10,n-1)));
+for(var n=1;n<6;n++){
+  console.log(Math.floor((num%Math.pow(10,n))/Math.pow(10,n-1)));
 }
