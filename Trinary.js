a50183a25066be81be70d4a1da64e317098cecc7
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-16 15:41:58 -0600

    Update Trinary.js

--- Trinary.js
+++ Trinary.js
@@ -1,11 +1,12 @@
 /*http://exercism.io/exercises/javascript/trinary/readme*/
 
 var num=102012;
-var power=1;
-var b10Total=0;
-for(var n=1;n<=6;n++){
-  b10Total+=(Math.floor((num%Math.pow(10,n))/Math.pow(10,n-1)))*power;
-  //console.log(power);
-  power*=3;
+function toBase10(num){
+  var power=1;
+  var b10Total=0;
+  for(var n=1;n<=6;n++){
+    b10Total+=(Math.floor((num%Math.pow(10,n))/Math.pow(10,n-1)))*power;
+    power*=3;
+  }
 }
 console.log(b10Total);
