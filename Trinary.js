83120dd62082ad474d1ce5c7475e076a08ec52a4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-16 10:11:50 -0600

    Update Trinary.js

--- Trinary.js
+++ Trinary.js
@@ -1,9 +1,11 @@
 /*http://exercism.io/exercises/javascript/trinary/readme*/
 
-var num=12345;
+var num=102012;
 var power=1;
-for(var n=1;n<6;n++){
-  console.log(Math.floor((num%Math.pow(10,n))/Math.pow(10,n-1)));
-  console.log(power);
+var b10Total=0;
+for(var n=1;n<=6;n++){
+  b10Total+=(Math.floor((num%Math.pow(10,n))/Math.pow(10,n-1)))*power;
+  //console.log(power);
   power*=3;
 }
+console.log(b10Total);
