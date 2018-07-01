778ab9d5568ae55ecfddfdd117f47c7df560622b
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-06-30 23:56:14 -0500

    Update Binary Dot Counter.js

--- Binary Dot Counter.js
+++ Binary Dot Counter.js
@@ -3,7 +3,7 @@ So I made my own binary dot counter/timer*/
 
 var canvas=document.getElementById("myCanvas");
 var cns=canvas.getContext("2d");
-cns.fillStyle="#FF0000";
+cns.fillStyle="#000000";
 for(var n=0;n<9;n++){
   cns.fillRect(n*50,0,5,150);
 }
