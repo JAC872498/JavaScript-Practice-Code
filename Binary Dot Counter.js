8c3e640deac253f88a0c0e8c7dea0e3f6eec357e
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-01 23:49:12 -0500

    Update Binary Dot Counter.js

--- Binary Dot Counter.js
+++ Binary Dot Counter.js
@@ -10,13 +10,13 @@ for(var n=0;n<9;n++){
   cns.fillRect(n*50,0,5,150);
 }
 for(var n=0;n<9;n++){
-	cns.fillRect(0,n*25,355,5);
+	cns.fillRect(0,n*25,405,5);
 }
 for(var n=0;n<2**9;n++){
 	count+=1;
     
 }
-for(var n=0;n<81;n++){
-	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);
-	cns.fillRect((Math.floor(n/9)*50)+5 , ((n%9)*25)+5 , 45,20);
+for(var n=0;n<48;n++){
+	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);#Gives random color
+	#cns.fillRect((Math.floor(n/6)*50)+5 , ((n%8)*25)+5 , 45,20);
 }
