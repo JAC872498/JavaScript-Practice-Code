097e0eea674c546edd8bf0d455c89c8164da73a9
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-02 00:13:26 -0500

    Update Binary Dot Counter.js

--- Binary Dot Counter.js
+++ Binary Dot Counter.js
@@ -8,15 +8,15 @@ var cns=canvas.getContext("2d");
 cns.fillStyle="#000000";
 for(var n=0;n<9;n++){
   cns.fillRect(n*50,0,5,150);
-}
-for(var n=0;n<9;n++){
-	cns.fillRect(0,n*25,405,5);
+  cns.fillRect(0,n*25,405,5);
 }
 for(var n=0;n<2**9;n++){
 	count+=1;
     
 }
-for(var n=0;n<48;n++){
-	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);#Gives random color
-	#cns.fillRect((Math.floor(n/6)*50)+5 , ((n%8)*25)+5 , 45,20);
+for(var n=0;n<8;n++){
+	for(var n2=0;n2<6;n2++){
+    	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);
+        cns.fillRect((n*50)+5,(n2*25)+5,45,20);
+	}
 }
