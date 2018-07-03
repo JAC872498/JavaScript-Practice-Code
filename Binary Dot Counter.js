1ae20007c09342a2079af5a1b3fa4f1fdbe5ea94
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-02 21:30:56 -0500

    Update Binary Dot Counter.js

--- Binary Dot Counter.js
+++ Binary Dot Counter.js
@@ -8,19 +8,8 @@ function wait(){
   do{d2=new Date();}
   while(d2-d1<1000);
 }
-
-//var count=11;
-var binary;
-var canvas=document.getElementById("myCanvas");
-var cns=canvas.getContext("2d");
-cns.fillStyle="#000000";
-for(var n=0;n<9;n++){
-  cns.fillRect(n*50,0,5,150);
-  cns.fillRect(0,n*25,405,5);
-}
-
-for(var count=0;count<9;count++){
-binary=(count.toString(2)).split("");
+function update(){
+	binary=(count.toString(2)).split("");
 for(b in binary){
 	if(binary[b]==1){
     	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);
@@ -31,9 +20,14 @@ for(b in binary){
 }
 }
 
-/*for(var n=0;n<8;n++){
-	for(var n2=0;n2<6;n2++){
-    	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);
-        cns.fillRect((n*50)+5,(n2*25)+5,45,20);
-	}
-}*/
+var count=11;
+var binary;
+var canvas=document.getElementById("myCanvas");
+var cns=canvas.getContext("2d");
+cns.fillStyle="#000000";
+for(var n=0;n<9;n++){
+  cns.fillRect(n*50,0,5,150);
+  cns.fillRect(0,n*25,405,5);
+}
+
+update();
