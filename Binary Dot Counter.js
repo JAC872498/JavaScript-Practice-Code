a0bacf11c02dbce41c6525efd8233ec7e4f0d39d
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-07-02 21:38:45 -0500

    Update Binary Dot Counter.js

--- Binary Dot Counter.js
+++ Binary Dot Counter.js
@@ -9,6 +9,11 @@ function wait(){
   while(d2-d1<1000);
 }
 function update(){
+  cns.clearRect(0,0,405,155);
+  for(var n=0;n<9;n++){
+    cns.fillRect(n*50,0,5,150);
+    cns.fillRect(0,n*25,405,5);
+  }
 	count+=1;
 	binary=(count.toString(2)).split("");
 	for(b in binary){
@@ -21,22 +26,10 @@ function update(){
 	}
 }
 
-var count=2;
+var count=0;
 var binary;
 var canvas=document.getElementById("myCanvas");
 var cns=canvas.getContext("2d");
 cns.fillStyle="#000000";
-for(var n=0;n<9;n++){
-  cns.fillRect(n*50,0,5,150);
-  cns.fillRect(0,n*25,405,5);
-}
 
 run=setInterval(update(),1000);
-
-
-/*for(var n=0;n<8;n++){
-	for(var n2=0;n2<6;n2++){
-    	cns.fillStyle="#"+((1<<24)*Math.random()|0).toString(16);
-        cns.fillRect((n*50)+5,(n2*25)+5,45,20);
-	}
-}*/
