614e6cfa5f820ddc25c592174ea2d222fe1222f7
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-05-17 22:54:53 -0500

    Added .push and .shift to merge()

--- Recursive Merging.js
+++ Recursive Merging.js
@@ -1,10 +1,14 @@
 /*A little recursive method I created do combine 2 lists recursively.*/
 
 function merge(list1, list2, rtnList){
-  if(list1.length==0 && list2.length==0){
+  if(list1.length==0 && list2.length==0){//Both lists empty
      return(rtnList);
-  }else if(list1.length==0){
-  }else{
+  }else if(list1.length!=0){//List 1 not empty
+    rtnList.push(list1[0]);
+    list1.shift();
+  }else{//List 1 empty and 
+    rtnList.push(list2[0]);
+    list2.shift();
   }
   return(merge(list1,list2,rtnList));
 }
