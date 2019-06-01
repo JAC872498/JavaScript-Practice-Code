dcc83c55daffcd44ee907b3048a08796d0a02456
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-05-31 23:59:45 -0500

    Changed some comments

--- Recursive Merging.js
+++ Recursive Merging.js
@@ -1,4 +1,4 @@
-/*A little recursive method I created do combine 2 lists recursively.*/
+/*A little recursive method I created to combine 2 lists recursively.*/
 
 function merge(list1, list2, rtnList){//Main merge method
   if(list1.length==0 && list2.length==0){//Both lists empty
